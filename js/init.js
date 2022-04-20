7c8b02b5ca0b50d9f77fd4a92f38b5241f1da9c3
Peter Heisig <kontakt@pehei.de>
2022-04-20 10:10:18 +0200

    reset

--- js/init.js
+++
@@ -1,8 +0,0 @@
-(function(){
-var allContentPs = document.querySelectorAll('p');
-for(var i = 0; i < allContentPs.length; i ++){
-allContentPs[i].classList.add('hyphenate');
-//assuming you have classList support (ie, IE10+); otherwise...
-//allContentPs[i].className += "hyphenate";
-}
-})();
